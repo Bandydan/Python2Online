1675568e6806fb588ab476723e870319049edf07
Bandydan <bandydan@gmail.com>
2022-08-02 20:01:33 +0300

    message for you

---
+++ bankomat.py
@@ -0,0 +1,17 @@
+#Банкомат выдает сумму максимально возможными купюрами
+
+# banknotes = [1000, 500, 200, 100, 50, 20, 10]
+banknotes = map(int, input("Please provide banknotes list divided by spaces: ").split())
+amount = int(input("Please provide the amount of grn you want to get: "))
+
+if amount % 10:
+	print("Error: wrong amount entered!")
+	exit()
+
+for nominal in banknotes:
+	print(nominal)
+	if amount // nominal:
+		print(' -> ' + str(amount // nominal))
+		amount %= nominal
+	if not amount:
+		exit()
